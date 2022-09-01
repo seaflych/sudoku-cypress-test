0b2b93697bbc7d4c096a773f3ce04cccd3b8cfb2
Haifei Zhang <haifei.zhang@ti8m.ch>
2022-09-01 15:24:21 +0200

    add prettier

--- cypress/e2e/hint.cy.ts
+++ cypress/e2e/hint.cy.ts
@@ -1,14 +1,14 @@
 /// <reference types="cypress" />
 
-describe("hint", () => {
-  it("solve the game", () => {
-    cy.visit("/");
-    cy.get(".game__cell")
-      .not(".game__cell--filled")
+describe('hint', () => {
+  it('solve the game', () => {
+    cy.visit('/');
+    cy.get('.game__cell')
+      .not('.game__cell--filled')
       .each(($el) => {
         cy.wrap($el).click();
-        cy.get(".status__action-hint").click();
+        cy.get('.status__action-hint').click();
       });
-    cy.get(".overlay__text").contains("You solved it").should("be.visible");
+    cy.get('.overlay__text').contains('You solved it').should('be.visible');
   });
 });
