5b30b2666c62c033f809a182ce9804345cbae9db
Haifei Zhang <haifei.zhang@ti8m.ch>
2022-09-01 15:25:00 +0200

    add modes, use alias

---
+++ cypress/e2e/modes.cy.ts
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('modes', () => {
+  it('shows different number of filled cells', () => {
+    cy.visit('/');
+    cy.get('select[name="status__difficulty-select"]').should('have.value', 'Easy');
+    cy.get('.game__cell--filled')
+      .should('have.length.greaterThan', 40)
+      .and('have.length.lessThan', 81)
+      .its('length')
+      .as('easyN');
+
+    cy.get('select[name="status__difficulty-select"]').select('Medium');
+    cy.get('.game__cell--filled').should('have.length.lessThan', 81).its('length').as('mediumN');
+
+    cy.get('select[name="status__difficulty-select"]').select('Hard');
+    cy.get('.game__cell--filled')
+      .should('have.length.lessThan', 81)
+      .its('length')
+      .as('hardN')
+      .then(function () {
+        expect(this.easyN, 'easy').to.be.greaterThan(this.mediumN);
+        expect(this.mediumN, 'medium').to.be.greaterThan(this.hardN);
+      });
+  });
+});
