9b6867a800ce20f7362da92e43662da455272ba9
Haifei Zhang <haifei.zhang@ti8m.ch>
2022-09-01 14:51:30 +0200

    add timer test, using for loop like JS

---
+++ cypress/e2e/timer.cy.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("Timer", () => {
+  it("show timer running 10 seconds ", () => {
+    cy.visit("/");
+    for (let i = 1; i < 10; i++) {
+      cy.contains(".status__time", `00:0${i}`);
+    }
+  });
+});
