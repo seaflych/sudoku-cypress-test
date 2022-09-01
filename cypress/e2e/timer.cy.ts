3f2e816577905ce629362f1e3a1f12d1f855f92d
Haifei Zhang <haifei.zhang@ti8m.ch>
2022-09-01 15:52:40 +0200

    use colock and tick

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -7,4 +7,15 @@ describe('Timer', () => {
       cy.contains('.status__time', `00:0${i}`);
     }
   });
+
+  it.only('shows minutes and seconds since the game starts', () => {
+    // use it before
+    cy.clock();
+    cy.visit('/');
+    cy.contains('.status__time', '00:00');
+    cy.tick(180_000);
+    cy.contains('.status__time', '03:00');
+    cy.tick(520_000);
+    cy.contains('.status__time', '11:40');
+  });
 });
